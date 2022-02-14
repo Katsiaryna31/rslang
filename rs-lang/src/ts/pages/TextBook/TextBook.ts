e00a7680e28bfc760b6521d4a8bdbd8899f223be
Katsiaryna31 <katia310790@mail.ru>
2022-02-14 22:08:40 +0300

    feat: add link on game from vocabulary

--- rs-lang/src/ts/pages/TextBook/TextBook.ts
+++ rs-lang/src/ts/pages/TextBook/TextBook.ts
@@ -1,7 +1,7 @@
 import Component from "../../common/Component";
 import Page from "../../common/Page";
 import { LocalStorageKey } from "../../settings";
-import audioCallPage from "../AudioCallPage/AudioCallPage";
+import { AudioCallPage } from "../AudioCallPage/AudioCallPage";
 import { WordData } from "./TextBookModel";
 import TextBookPresenter from "./TextBookPresenter";
 import WordElem from "./WordElem";
@@ -163,11 +163,15 @@ export class TextBookPage extends Page {
     const sptintBtn = new Component('li', 'location-select__item', `Спринт`).node;
     list.append(audioсallBtn, sptintBtn);
 
-    //audioсallBtn.onclick = () => {
-    //  const location = this.getLocation();
-    //  const audioCallGame = new AudioCallPage(location.group, location.page);
-    //  audioCallPage.render();
-    //}
+    audioсallBtn.onclick = () => {
+      const root = document.querySelector('#root') as HTMLElement;
+      const location = this.getLocation();
+      const audioCallGame = new AudioCallPage((location.group - 1).toString(), (location.page - 1).toString());
+      root.innerHTML = '';
+      history.pushState('', '', '#/audiocall');
+      const pageElement = audioCallGame.render();
+      root.append(pageElement);
+     }
 
     this.gameSelect.onclick = (e) => {
       list.classList.add('active');
